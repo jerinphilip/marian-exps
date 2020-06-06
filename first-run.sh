60b2bb2f2dce9ef725f427c24731d9a5975e9f50
Jerin Philip <jerin.philip@students.iiit.ac.in>
2020-06-06 21:53:41 +0530

    Understanding a little more options

--- first-run.sh
+++ first-run.sh
@@ -28,8 +28,10 @@ function create_vocabs {
 
 
 marian \
+    --model pib.en-hi.npz   \
     --train-sets $DATA_DIR/pib/en-hi/train.{en,hi} \
+    --valid-sets $DATA_DIR/mkb/en-hi/mkb.{en,hi} \
     --vocabs $VOCABS_DIR/pib_en.spm $VOCABS_DIR/pib_hi.spm \
     --dim-vocabs 8000 8000 \
     --devices 1 2 3     \
-    --workspace=8000 --mini-batch-fit
+    --workspace=10000 --mini-batch-fit
---
+++ set-paths.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+# Setup env
+MARIAN_INSTALLED_DIR=$(realpath ../env)
+
+export PATH="$MARIAN_INSTALLED_DIR/bin:$PATH".
+export LD_LIBRARY_PATH="$MARIAN_INSTALLED_DIR/lib:$LD_LIBRARY_PATH".
