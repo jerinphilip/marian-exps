5718fa61ab943160960f8f0cba12d9f9b028662d
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-06-06 19:07:30 +0530

    Adding first set

---
+++ first-run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -x
+
+# Setup env
+MARIAN_INSTALLED_DIR=$(realpath ../env)
+
+export PATH="$MARIAN_INSTALLED_DIR/bin:$PATH".
+export LD_LIBRARY_PATH="$MARIAN_INSTALLED_DIR/lib:$LD_LIBRARY_PATH".
+
+DATA_DIR="../data"
+VOCABS_DIR=$DATA_DIR/vocabs
+VOCAB_SIZE=8000
+
+function create_vocabs {
+    mkdir -p $VOCABS_DIR
+    spm_train --input=$DATA_DIR/pib/en-hi/train.en\
+        --model_prefix=$VOCABS_DIR/pib_en \
+        --vocab_size=$VOCAB_SIZE
+
+    spm_train --input=$DATA_DIR/pib/en-hi/train.hi\
+        --model_prefix=$VOCABS_DIR/pib_hi \
+        --vocab_size=$VOCAB_SIZE
+
+    mv $VOCABS_DIR/pib_en.{model,spm}
+    mv $VOCABS_DIR/pib_hi.{model,spm}
+
+}
+
+
+marian \
+    --train-sets $DATA_DIR/pib/en-hi/train.{en,hi} \
+    --vocabs $VOCABS_DIR/pib_en.spm $VOCABS_DIR/pib_hi.spm \
+    --dim-vocabs 8000 8000 \
+    --devices 1 2 3     \
+    --workspace=8000 --mini-batch-fit
